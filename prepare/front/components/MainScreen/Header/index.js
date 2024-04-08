589f78b6095ecfa9393280684780ff83b2aaedf2
ParkDongGyu <idqkrid@naver.com>
2024-04-08 21:56:09 +0900

    getServierside commit

--- prepare/front/components/MainScreen/Header/index.js
+++ prepare/front/components/MainScreen/Header/index.js
@@ -5,6 +5,10 @@ import Link from 'next/link';
 // redux
 import { useDispatch, useSelector } from 'react-redux';
 import { LOG_OUT_REQUEST } from '../../../reducers/user';
+import { LOAD_MY_INFO_REQUEST } from "../../../reducers/user";
+import wrapper from "../../../store/configureStore";
+import { END } from "redux-saga";
+import axios from "axios";
 
 const HeadMain = () => {
   const dispatch = useDispatch();
@@ -75,5 +79,21 @@ const HeadMain = () => {
     </div>
   )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+
+    context.store.dispatch(END);
+    await context.store.sagaTask.toPromise();
+  }
+);
 
 export default HeadMain;
