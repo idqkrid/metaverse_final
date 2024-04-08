0de9af118715f023427422e992e69b80883325b5
ParkDongGyu <idqkrid@naver.com>
2024-04-08 17:55:13 +0900

    update two

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
-// const cors = require('cors');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser');
-// const path = require('path');
+const cors = require("cors");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 // const postRouter = require('./routes/post')
 // const userRouter = require('./routes/user');
@@ -13,13 +13,13 @@ const express = require('express')
 // const metaRouter = require('./routes/meta');
 
 // const db = require("./models");
-// const passportConfig = require("./passport");
-// const passport = require("passport");
-// const dotenv = require("dotenv");
+const passportConfig = require("./passport");
+const passport = require("passport");
+const dotenv = require("dotenv");
 
-// const webSocket = require("./socket");
+const webSocket = require("./socket");
 
-// dotenv.config();
+dotenv.config();
 const app = express();
 
 // db.sequelize
@@ -30,28 +30,32 @@ const app = express();
 //   .catch(console.error);
 // passportConfig();
 
-// app.use(cors({
-//   origin: 'http://localhost:3060',
-//   credentials: true, // 쿠키를 같이 전달하고 싶으면 true
-//   webSocket: true,
-// }));
+app.use(
+  cors({
+    origin: "http://localhost:3060",
+    credentials: true, // 쿠키를 같이 전달하고 싶으면 true
+    webSocket: true,
+  })
+);
 
-// app.use('/', express.static(path.join(__dirname, 'uploads')))
-// app.use(express.json());
-// app.use(express.urlencoded({extends: true})) // 프론트에서 보낸 데이터를 req.body 에다가 넣어주는 역할
+app.use("/", express.static(path.join(__dirname, "uploads")));
+app.use(express.json());
+app.use(express.urlencoded({ extends: true })); // 프론트에서 보낸 데이터를 req.body 에다가 넣어주는 역할
 
-// app.use(cookieParser());
-// app.use(session({
-//   saveUninitialized: false,
-//   resave: false,
-//   secret: process.env.COOKIE_SECRET,
-// }));
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(cookieParser());
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/', (req, res) => {
-  res.send('hello express');
-})
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
 
 // app.use('/post', postRouter);
 // app.use('/posts', postsRouter);
@@ -64,14 +68,13 @@ app.get('/', (req, res) => {
 // next(err) 인경우 실행
 app.use((err, req, res, next) => {
   // 에러가 났을경우 보여주는 페이지
-})
+});
 
-
-server = app.listen(80, () => {
+const server = app.listen(80, () => {
   console.log("서버 실행중..");
 });
 
-// webSocket(server, app);
+webSocket(server, app);
 
 /**
  * app.get -> 가져오다
